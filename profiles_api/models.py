599c7b5a6fbfe1ad40bcb51a885525afcfd5cc40
Felix Edesa <felix.edesa@aastu.edu.et>
2024-09-06 13:56:13 +0300

    Added custom user profile model,manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile with given email, name and password"""
+        if not email:
+            raise ValueError("Email must be provided")
+        if not name:
+            raise ValueError("Name must be provided")
+
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, name, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database Model for users in the System"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """ Return the full name of the user"""
+        return self.name
+
+    def get_short_name(self):
+        """Return the short name of the user"""
+        return self.name
+
+    def __str__(self):
+        """Return a string representation of the user"""
+        return self.email
+
+
+
+
+
+
+
